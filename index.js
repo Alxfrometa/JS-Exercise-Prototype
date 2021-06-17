d89550c3ee947b49eb8adceda696c79fc9cc7618
AlexFrometa <you@example.com>
2021-06-17 10:18:40 -0500

    fully completed

--- index.js
+++ index.js
@@ -75,10 +75,10 @@ console.log(alex);
   */
   
  function Car(model, milesPerGallon) {
-    this.model = model
-    this.milesPerGallon = milesPerGallon
-    this.tank = 0
-    this.odemeter = 0
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    this.tank = 0;
+    this.odemeter = 0;
   }
   Car.prototype.fill = function(gallons){
     this.tank = this.tank + gallons;
@@ -91,18 +91,22 @@ console.log(alex);
       - Besides the methods on Person.prototype, babies have the ability to `.play()`:
           + Should return a string "Playing with x", x being the favorite toy.
   */
- function Baby(favoriteToy) {
-    
+ function Baby(name, age, favoriteToy) {
+    Person.call(this, name, age);
+    this.favoriteToy = favoriteToy;
   }
- 
+ Baby.prototype = Object.create(Person.prototype)
+ Baby.prototype.play = function(){
+    return  `Playing with ${this.favoriteToy}`
+ }
   
   /* 
     TASK 4
     In your own words explain the four principles for the "this" keyword below:
-    1. 
-    2. 
-    3. 
-    4. 
+    1. Window Binding - this is an error when not givien the proper context
+    2. Implicit Binding - when invoked to look left of the dot is what this refers to
+    3. Explicit Binding - explicitly says what to do with: .call, .apply, .bind
+    4. New Binding - when functions created as a constructor, this points to the new object
   */
   
   
